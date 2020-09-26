feb57a1b85ed7cc8383187b98ece7af9d4d0a8cd
Austin Varnon <austinvarnon@gmail.com>
2020-09-25 22:35:43 -0500

    checked a function

--- movie-app/src/components/Movie-List.js
+++ movie-app/src/components/Movie-List.js
@@ -4,11 +4,10 @@ import MovieCard from "./Movie-Card";
 
 function MovieList ({ results }) {
 
-    //TODO: figure out what is going on with map function on line 11, breaking when typing search into searchbar
-
         //use () instead of {} so JSX can return HTML syntax
     return (
         <section className="results">
+            ////TODO: figure out what is going on with map function on line 11, breaking when typing search into searchbar
             {results.map(result => (
                 <MovieCard result={result} />
             ))}
