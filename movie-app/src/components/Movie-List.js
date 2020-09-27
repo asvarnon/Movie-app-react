8aa0700f0e6aa687330b7ddf27f90114188c00c0
Austin Varnon <austinvarnon@gmail.com>
2020-09-27 16:35:33 -0500

    Tried conditional to negate possibiliy of async data.

--- movie-app/src/components/Movie-List.js
+++ movie-app/src/components/Movie-List.js
@@ -2,15 +2,23 @@ import React from "react";
 import MovieCard from "./Movie-Card";
 
 
-function MovieList (props) {
+function MovieList ({ results }) {
 
         //use () instead of {} so JSX can return HTML syntax
     return (
         <section className="results">
             //TODO: figure out what is going on with map function on line 11, breaking when typing search into searchbar
-            {props.results.map(result => (
-                <MovieCard result={result} />
+            {/*{*/}
+            {/*    if(results){*/}
+            {/*        results.map(result => (*/}
+            {/*            <MovieCard result={result}*/}
+            {/*    ))}*/}
+            {/*}*/}
+
+            {results.map(result => (
+            <MovieCard result={result} />
             ))}
+
         </section>
     )
 }
